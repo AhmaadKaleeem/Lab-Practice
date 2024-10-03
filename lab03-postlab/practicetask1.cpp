b743cec30ada9ad67a31c2074a6767f5f31d6672
Ahmad Kaleem Bhatti <ahmadkaleeem1@gmail..com>
2024-10-04 02:12:15 +0500

    Add Practice Task 01 extension

---
+++ lab03-postlab/practicetask1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+                int num1,num2;
+cout << "Enter the First Number : ";
+   cin >> num1;
+cout << "Enter the Second Number : ";
+   cin >> num2;
+cout << "You have Entered " << num1 << " as First Number and " << num2 << " as Second Number""\n" ;
+   if (num1<num2) {
+cout << "Second Number is Greater";
+                  }
+   else{
+cout << "First Number is Greater";
+       }               
+
+}
