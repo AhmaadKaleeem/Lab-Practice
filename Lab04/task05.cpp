c810c6d9c191b5cc8b7324bc65229844deb120c6
Ahmad Kaleem Bhatti <ahmadkaleeem1@gmail..com>
2024-10-08 02:59:12 +0500

    Added changes  For Lab Task 05

--- Lab04/task05.cpp
+++ Lab04/task05.cpp
@@ -4,32 +4,40 @@
 using namespace std;
 
 int main() {
-    int num;
-    cout << "Enter an integer : ";
-    while (true) {
-        cin >> num;
-        if (num < 0)
-        {
-            cout << "Plesse, Enter a Positive Integer.";
-            return 0;
-        }
-        else if (num == 1)
-        {
-            cout << "This is not a Prime Number";
-            break;
-        }
-
-        else if (num % num == 0)
-        {
-            cout << "This is a Prime Number.";
-
-        }
-        else {
-            cout << "This is not a Prime Number";
-        }
+    int num,isprime=1;
+    cout << "Enter an Integer : ";
+    cin >> num;
+    if (num < 0)
+    {
+        cout << "Plesse, Enter a Positive Integer.";
+        return 0;
     }
+    else if (num == 1)
+    {
+        cout << num << " is not a Prime Number";
+        return 0;
+    }
+     
+    for (int i = 2; i <= num/2; i++)
+{
+            if (num % i == 0)
+            {
+                isprime = 0;
+                break;
+            }
+            
+   
+}
+           if (isprime == 1)
+           {
+               cout << num << " is a Prime Number";
+           }
+           else 
+           {
+               cout << num << " is not a Prime Number";
+           }
 
-
-
+    
 
 }
+
