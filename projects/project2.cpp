530f23dc681c21904a020d515a33d6d739e62430
Ahmad Kaleem Bhatti <ahmadkaleeem1@gmail..com>
2024-10-26 01:45:57 +0500

    Added Error Handling in amounts for deposit and withdrwal and enhanced the code

--- projects/project2.cpp
+++ projects/project2.cpp
@@ -31,37 +31,55 @@ int main()
 
         // using if else to use the desired option 
         // Creating Account
-        if (option == 1)
-        {
-            cout << "\t\t\t\t""Welcome to Sharif Bank (Ltd) " << "\n";
-            cout << "Enter Your Name > ";
-            cin >> name;
-            cout << "Enter Your Identification Number > ";
-            cin >> cnic;
-            cout << "Enter Your Six Digits Account Number > ";
-            cin >> notfixedacc;
+        // using if else to use the desired option
+if (option == 1) 
+{
+    cout << "\t\t\t\t""Welcome to Sharif Bank (Ltd) " << "\n";
+    cout << "Enter Your Name > ";
+    cin >> name;
 
+// Taking Input and Making sure that entered Identification number must be of 5 Digits 
+    cout << "Enter Your 5 Digit National Identification Number > ";
 
-            // Making sure that entered account number is not greater than or less than six
-            if (notfixedacc >= 7 && notfixedacc < 6)
-            {
-                cout << "Error! Account Number Cannot Exceed or Less Than Six Digits ";
-            }
-            else
-            {
-                cout << "Congratulations " << name << "! You Have Successfully Created Account With Account  Number "
-                    << fixedaccnbr << notfixedacc << " Against Identification Number " << cnic << ".\n\n";
-            }
-        }
+    if (notfixedacc > 99999 && notfixedacc < 10000)
+    {
+        cout << "Error! Account Number Cannot Exceed or Less Than Six Digits ";
+    }
+    else 
+    {
+        cin >> cnic;
+    }
+
+// Taking Input and Making sure that entered account number is not greater than or less than six
+    cout << "Enter Your Six Digits Account Number > ";
+    cin >> notfixedacc;
+
+    if (notfixedacc >999999 && notfixedacc<100000)
+    {
+        cout << "Error! Account Number Cannot Exceed or Less Than Six Digits ";
+    }
+    else
+    {
+        cout << "Congratulations " << name << "! You Have Successfully Created Account With Account Number " 
+             << fixedaccnbr << notfixedacc << " Against Identification Number " << cnic << "\n\n";
+    }
+}
 
         // Depositing Money
         else if (option == 2)
         {
             cout << "Enter The Amount To Deposit Into Your Account : Rs. ";
-            cin >> deposit;
-            balance = balance + deposit;
-            cout << "Your Transaction is Successfully Completed ! " << "\n";
-            cout << "Your New Balance is  Rs. " << balance << "\n\n";
+            if (deposit>=0)
+            {
+                cin >> deposit;
+                balance = balance + deposit;
+                cout << "Your Transaction is Successfully Completed ! " << "\n";
+                cout << "Your New Balance is  Rs. " << balance << "\n\n";
+            }
+            else
+            {
+                cout << "Please ! Enter Amount in Positive Value \n\n";
+            } 
         }
 
         // Withdrawal Money
@@ -69,34 +87,41 @@ int main()
         {
             cout << "Enter The Amount To Withdrawal From Your Account : Rs. ";
             cin >> withdrawal;
-            if (balance > withdrawal)
+            if (balance > withdrawal && withdrawal > 0)
             {
                 balance = balance - withdrawal;
                 cout << "Your Transaction is Successfully Completed ! " << "\n";
                 cout << "Your New Balance is  Rs. " << balance << "\n\n";
             }
-            else
+            else if (balance < withdrwal )
             {
                 cout << "Sorry! Your Account Doesn't Have Sufficient Balance. ";
             }
+             else 
+             {
+                cout << "Invalid Amount. Please Enter a Positive Value.\n\n";
+             }
             
         }
 
         // Account Information
         else if (option == 4)
         {
-            cout << "Your Account Number is " <<fixedaccnbr <<notfixedacc <<"\n";
-            cout << "Your Account Balance is Rs. " << balance <<"\n";
+            cout << "\t\t\t\t Account Information \n";
+            cout << "Account Holder Name : " << name << "\n";
+            cout << "Account Number: " << fixedaccnbr << notfixedacc << "\n";
+            cout << "Identification Number : " << cnic << "\n";
+            cout << "Current Balance: " << balance << "\n\n";
         }
 
         // Exit
         else if (option == 5) {
-            return 0;
+          cout << "Exiting the System. Thank You for Using Sharif Bank (Ltd).\n"
         }
 
 
         option++;
     }
-
+  return 0;
 }
 
