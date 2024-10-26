50ce30c738380733e4f4b1fd8ab98fc830dd62af
Ahmad Kaleem Bhatti <ahmadkaleeem1@gmail..com>
2024-10-27 03:06:40 +0500

    Added Random Generating Number System and Validate The Input Data Type

--- projects/project2.cpp
+++ projects/project2.cpp
@@ -1,6 +1,6 @@
 // Project: Bank Account Management System
 /*Features:
-Create an Account : User enters their initial deposit and gets a new account number.
+Create an Account : User enters their details,deposit and gets a new account number.
 Deposit Money : User can deposit money into their account.
 Withdraw Money : User can withdraw money, but only if the balance allows.
 Display Account Information : Shows account number, balance, and recent transactions. */
@@ -8,7 +8,28 @@ Display Account Information : Shows account number, balance, and recent transact
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <ctime>
+# include <stdlib.h> //Create Random Numbers
+#include <limits> // To Handle Wrong Data Type Input
 using namespace std;
+
+int checkinputtype(const string& prompt) {
+    int input;
+    while (true) {
+        cout << prompt;
+        cin >> input;
+        // Check if input is valid
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "\t\t\t\tInvalid input. Please Enter an Integer.\n\n";
+        }
+        else {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            return input;
+        }
+    }
+}
 
 void savetransactiondetails(const string& name, long cnic, long notfixedacc, long balance) {
     ofstream recenttrans("Recent Transactions.txt", ios::app);
@@ -23,6 +44,7 @@ void savetransactiondetails(const string& name, long cnic, long notfixedacc, lon
         cout << "There is an Error in Opening The File. ";
     }
 }
+
 void withdrwawalamount(long withdrawal, long balance) {
     ofstream recenttrans("Recent Transactions.txt", ios::app);
     // Check if the file opened successfully
@@ -36,6 +58,7 @@ void withdrwawalamount(long withdrawal, long balance) {
         cout << "There is an Error in Opening The File. \n";
     }
 }
+
 void depositamount(long deposit, long balance) {
     ofstream recenttrans("Recent Transactions.txt", ios::app);
     // Check if the file opened successfully
@@ -49,16 +72,13 @@ void depositamount(long deposit, long balance) {
         cout << "There is an Error in Opening The File. \n";
     }
 }
-   
-
-
 
 
 int main()
 {
     // Declaring Variables
    // For making a fixed account number before 
-    long  deposit = 0, balance = 0, recent = 0, withdrawal= 0, option = 0, cnic = 0, notfixedacc;
+    long  deposit = 0, balance = 0, recent = 0, withdrawal = 0, option = 0, cnic = 0, notfixedacc;
     string name, fixedaccnbr = "05101324";
     ofstream recenttrans; //Using Filehandling
 
@@ -67,18 +87,15 @@ int main()
     cout << "-----------------------------------------------------------------------------------------------------------------------";
     cout << endl;
 
-
+    srand(time(0));
 
     while (option != 5)
     {
-        cout << " Please Select an Option  ! " << "\n\n";
+        cout << "Please Select an Option  ! " << "\n\n";
         cout << "1. Create New Account in Sharif Bank (Ltd) " << "\n" << "2. Deposit Money in your Account" << "\n"
             << "3. Withdrawl Money From Your Account " << "\n" << "4. View Account Information " << "\n"
             << "5. Exit " << "\n\n";
-        cout << "Enter the Selected Option > ";
-        cin >> option;
-        cout << "\n";
-
+        option = checkinputtype("Enter the Selected Option > ");
 
         // using if else to use the desired option 
         // Creating Account
@@ -86,15 +103,15 @@ int main()
         if (option == 1)
         {
             // Clear previous transactions 
-            recenttrans.open("Recent Transactions.txt", ios::trunc); recenttrans.close();
+            recenttrans.open("Recent Transactions.txt", ios::trunc);
+            recenttrans.close();
 
             cout << "\t\t\t\t""Welcome to Sharif Bank (Ltd) " << "\n";
             cout << "Enter Your Name (No Spaces) > ";
             cin >> name;
 
             // National ID Validation (5-Digit CNIC) with 4 Time Limit
-            cout << "Enter Your 5 Digit National Identification Number > ";
-            cin >> cnic;
+            cnic = checkinputtype("Enter Your 5 Digit National Identification Number > ");
             int tryagain = 4;
             while ((cnic > 99999 || cnic < 10000) && tryagain > 0)
             {
@@ -109,29 +126,24 @@ int main()
                 continue;
             }
 
-            // Taking Input and Making sure that entered account number is not greater than or less than six
-            cout << "Enter Your Six Digits Account Number > ";
-            cin >> notfixedacc;
+            balance = checkinputtype("Enter Your Intial Deposit > Rs.");
+          
+            // Generating Radnom Account Number
+            notfixedacc = rand() % 900000 + 100000;
 
-            if (notfixedacc > 999999 || notfixedacc < 100000)
-            {
-                cout << "Error! Account Number Cannot Exceed or Less Than Six Digits ";
-                cout << "\n\n";
-            }
-            else
-            {
-                cout << "Congratulations " << name << "! You Have Successfully Created Account With Account Number "
-                    << fixedaccnbr << notfixedacc << " Against Identification Number " << cnic << "\n\n";
+            // Account Created Successfully
+            cout << "Congratulations " << name << "! You Have Successfully Created Account With Account Number "
+                << fixedaccnbr << notfixedacc << " Against Identification Number " << cnic << "\n\n";
 
-                // Calling Function to Log Details into the File        
-                savetransactiondetails (name, notfixedacc, cnic, balance);
-            }
+            // Calling Function to Log Details into the File        
+            savetransactiondetails(name, notfixedacc, cnic, balance);
+
         }
 
         // Depositing Money
         else if (option == 2)
         {
-            cout << "Enter The Amount To Deposit Into Your Account : Rs. ";
+           deposit= checkinputtype("Enter The Amount To Deposit Into Your Account : Rs. ");
             if (deposit >= 0)
             {
                 cin >> deposit;
@@ -151,8 +163,7 @@ int main()
         // Withdrawal Money
         else if (option == 3)
         {
-            cout << "Enter The Amount To Withdrawal From Your Account : Rs. ";
-            cin >> withdrawal;
+            withdrawal= checkinputtype("Enter The Amount To Withdrawal From Your Account : Rs. ");
             if (balance > withdrawal && withdrawal > 0)
             {
                 balance = balance - withdrawal;
@@ -199,8 +210,8 @@ int main()
 
 
         }
-
-        // Exit
+        \
+            // Exit
         else if (option == 5)
         {
             cout << "Exiting the System. Thank You for Using Sharif Bank (Ltd).\n";
